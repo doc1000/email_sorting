fee1b1d7e827afea10a2ccaa47c130c98fb59933
doc1000 <doster1000@hotmail.com>
2018-04-19 07:18:54 -0600

    testing topic_classifier.py done

--- topic_classifier.py
+++ topic_classifier.py
@@ -23,7 +23,7 @@ class InquireBoulderData(object):
     '''
     Reads data from Inquire Boulder formatted files
     '''
-    def __init__():
+    def __init__(self):
         self.categories = {}
         self.categories_reversed = {}
         self.X = None
@@ -31,7 +31,7 @@ class InquireBoulderData(object):
         #self.data_filename= data_filename
         #self.topic_filename = topic_filename
 
-    def fit(self,data_filename=None,topic_filename=None)
+    def fit(self,data_filename=None,topic_filename=None):
         if data_filename == None:
             data_filename = 'data/Inquire_Boulder_All_requests_2017_with_details.csv'
         data_df = pd.read_csv(data_filename)
@@ -40,7 +40,7 @@ class InquireBoulderData(object):
             topic_filename = 'data/topics.csv'
         topics_df = pd.read_csv(topic_filename,sep=';')
 
-        df_c2 = data_df.set_index('Topic').join(topic_df.set_index('subcategory'))
+        df_c2 = data_df.set_index('Topic').join(topics_df.set_index('subcategory'))
         df_c2 = df_c2.reset_index()
         cats = df_c2['category'].unique()
         for i, cat in enumerate(cats):
@@ -49,23 +49,23 @@ class InquireBoulderData(object):
         df_c2['cat_num']=df_c2['category'].map(self.categories_reversed)
         self.X = df_c2.Description
         self.y = df_c2['cat_num']
-        return self,X,y
+        return self.X,self.y
 
 class EmailData(object):
     '''
     Reads data from Boulder City Council Email formatted files
     '''
-    def __init__():
+    def __init__(self):
         self.X = None
 
     def fit(self,data_filename=None):
         if data_filename == None:
             data_filename = 'data/jan_mar_2018.csv'
-        data_df = pd.read_csv(data_filename)
+        mail_df = pd.read_csv(data_filename)
         mail_df = mail_df[mail_df['Body'].isnull()==False]
         self.X = mail_df['Body']
 
-        return self,self.X
+        return self.X
 
 
 class LemmedCountVectorizer(CountVectorizer):
@@ -77,8 +77,8 @@ class LemmedCountVectorizer(CountVectorizer):
 
 
 class TopicClassifier(object):
-    def __init__():
-        self.model=None
+    def __init__(self):
+        self._model=None
         self.categories = {}
 
     def fit(self,X,y):
@@ -88,25 +88,44 @@ class TopicClassifier(object):
             ('clf', SGDClassifier(penalty='elasticnet',alpha=0.0001,loss='log',class_weight='balanced')),
         ])
         pipeline.fit(X,y)
-        self.model=pipeline
+        self._model=pipeline
         return self
 
-    def predict(X):
-        y_pred = model.predict(X)
+    def predict(self,X):
+        y_pred = self._model.predict(X)
         y_cats = np.vectorize(self.categories.get)(y_pred)
         return y_cats,y_pred
 
-    def save_model_to_pickle(filename=None):
+    def save_model_to_pickle(self,filename=None):
         if filename == None:
             filename = 'models/pipeline_to_SGD_logloss.p'
-        pickle.dump(pipeline,open(filename,'wb'))
+        pickle.dump(self._model,open(filename,'wb'))
 
-    def load_model_from_pickle(filename=None):
+    def load_model_from_pickle(self,filename=None):
         if filename == None:
             filename = 'models/pipeline_to_SGD_logloss.p'
-        self.model = pickle.load(open(filename,'rb'))
+        self._model = pickle.load(open(filename,'rb'))
 
-    def load_categories_from_pickle(filename=None):
+    def load_categories_from_pickle(self,filename=None):
         if filename == None:
             filename = 'data/categories_with_index.p'
         self.categories = pickle.load(open(filename, 'rb'))
+
+    def save_predictions_to_csv(self,filename=None):
+        if filename == None:
+            filename = 'data/predicted_category_log.csv'
+        mail_df.to_csv(filename)
+
+
+if __name__ == '__main__':
+    X_train,y_train = InquireBoulderData().fit()
+    model = TopicClassifier()
+    #model.fit(X_train,y_train)
+    model.load_model_from_pickle()
+    X = EmailData().fit()
+    y_categories, y_cat_nums = model.predict(X)
+
+
+
+#mail_df['predicted_category']=y_cats
+#mail_df.to_csv('data/predicted_category_log.csv')
