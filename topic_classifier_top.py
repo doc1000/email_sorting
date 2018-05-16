dbbce2e347878446d3a56dceaa3cd40baa163d64
doc1000 <doster1000@hotmail.com>
2018-05-16 08:03:32 -0600

    error checking predict, top words, similar

--- topic_classifier_top.py
+++ topic_classifier_top.py
@@ -98,6 +98,7 @@ class TopicClassifier(object):
 
     def fit(self,X,y):
         X = self.text_cleaning(X)
+        self.X = X
         pipeline = Pipeline([
             ('vect', LemmedCountVectorizer(stop_words='english',analyzer="word",max_df=0.5)),
             ('tfidf', TfidfTransformer(norm='l2')),
@@ -106,13 +107,16 @@ class TopicClassifier(object):
         pipeline.fit(X,y)
         self._model=pipeline
         self.vocabulary_ = self._model.named_steps['vect'].vocabulary_
+        self.load_categories_from_pickle()
         self._vect = model._model.named_steps['vect'].transform(X)
         self._tfidf_matrix = model._model.named_steps['tfidf'].transform(self._vect)
         return self
 
     def predict(self,X,return_type=1):
+        if type(X)!=list:
+            X = [X]
         X = self.text_cleaning(X)
-        self.X = X
+        #self.X = X
         y_pred = self._model.predict(X)
         y_cats = np.vectorize(self.categories.get)(y_pred)
         # if return_type == 1:
@@ -126,7 +130,7 @@ class TopicClassifier(object):
     def save_model_to_pickle(self,filename=None):
         if filename == None:
             filename = 'models/pipeline_to_SGD_logloss.p'
-        pickle.dump(self._modelopen(filename,'wb'))
+        pickle.dump(self._model,open(filename,'wb'))
 
     def load_model_from_pickle(self,filename=None):
         if filename == None:
@@ -154,7 +158,7 @@ class TopicClassifier(object):
         return docs
 
     def top_words(self,doc):
-        doc = [doc]
+        if type(doc)!=list: doc = [doc]
         #vect_mod = self._model.named_steps['vect']
         vect = self._model.named_steps['vect'].transform(doc)
         rev_d = {v:k for k,v in self.vocabulary_.items()}
@@ -166,7 +170,7 @@ class TopicClassifier(object):
 
     def similar_emails(self,doc):
         from sklearn.metrics.pairwise import cosine_similarity
-        doc = [doc]
+        if type(doc)!=list: doc = [doc]
         vect = model._model.named_steps['vect'].transform(doc)
         tf_matrix = model._model.named_steps['tfidf'].transform(vect)
         #vect_X = model._model.named_steps['vect'].transform(model.X)
@@ -176,8 +180,8 @@ class TopicClassifier(object):
         cossim = cosine_similarity(tf_matrix[:],self._tfidf_matrix[:])
         #cossim[0][index_d] = 0
         comp_docs = [(cossim.reshape(-1,1)[i][0],model.X[i]) for i in np.argsort(cossim*-1)[0][:5]]
-        print(comp_docs[0])
-
+        #print(comp_docs[0])
+        return comp_docs[0]
 
 def text_cleaning(docs):
     import re
@@ -189,38 +193,34 @@ def text_cleaning(docs):
     return docs
 
 def load_up_model():
-    X_train,y_train = InquireBoulderData().fit()
+    #X_train,y_train = InquireBoulderData().fit()
     model = TopicClassifier()
     #model.fit(X_train,y_train)
     model.load_model_from_pickle()
-    X = EmailData().fit()
-    y = model.predict(X)
+    #X = EmailData().fit()
+    #y = model.predict(X)
 
-    df = pd.DataFrame(model.X)
-    df['topic']=y
+    #df = pd.DataFrame(model.X)
+    #df['topic']=y
     #df.to_csv('data/results.csv')
-    return model, df
+    return model
 
 
 
 if __name__ == '__main__':
-    #similar messages
-    from sklearn.metrics.pairwise import cosine_similarity
-    model, _ = load_up_model()
-    doc = 'the city counsel needs to address ausault weapon control.  guns are killing our kids'
-    doc = [doc]
-    vect = model._model.named_steps['vect'].transform(doc)
-    tf_matrix = model._model.named_steps['tfidf'].transform(vect)
-    vect_X = model._model.named_steps['vect'].transform(model.X)
-    tf_matrix_X = model._model.named_steps['tfidf'].transform(vect_X)
+    X_train,y_train = InquireBoulderData().fit()
+    model = TopicClassifier()
+    model.fit(X_train,y_train)
+    model.save_model_to_pickle('models/pipeline_to_SGD_logloss_05132018.p')
+    #del model
+    #model = TopicClassifier()
+    #model.load_model_from_pickle('models/pipeline_to_SGD_logloss_05132018.p')
+    #model = load_up_model()
+    doc = 'The streets in this town are paved with potholes.  we need better snow removal.'
+    print(model.predict(doc))
+    print(model.top_words(doc))
+    print(model.similar_emails(doc))
 
-    index_d = 0
-    cossim = cosine_similarity(tf_matrix[:],tf_matrix_X[:])
-    #cossim[0][index_d] = 0
-    comp_docs = [(cossim.reshape(-1,1)[i][0],model.X[i]) for i in np.argsort(cossim*-1)[0][:5]]
-    print(comp_docs[0])
-    #i might need to append any unknown words to the existing tfidf.  maybe
-    #or could just transform doc directly using named steps... der... already fit.
 
 #mail_df['predicted_category']=y_cats
 #mail_df.to_csv('data/predicted_category_log.csv')
